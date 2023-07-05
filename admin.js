4d54164b793e4b7a787467f30e65cab9be9e8ee7
= <bksuman920@gmail.com>
2023-07-05 17:08:21 +0545

    Resopnsive completion

--- admin.js
+++ admin.js
@@ -2,6 +2,7 @@ var lessthan = document.getElementById("less-then")
 var greaterthan = document.getElementById("greater-then")
 
 var navBar = document.getElementById("navbar")
+var leftUl = document.getElementById("left-ul")
 
 var navName = document.querySelectorAll(".nav-name")
 var navItems = document.querySelectorAll(".nav__items")
@@ -9,6 +10,7 @@ var navItems = document.querySelectorAll(".nav__items")
 var leftNavBar = document.getElementById("left-navbar")
 
 var navBrand = document.getElementById("navbar-brands")
+var navBrands = document.getElementById("navbar-brandss")
 
 var sectionContent = document.getElementById("section-content")
 
@@ -35,23 +37,36 @@ lessthan.addEventListener("click", () => {
 
 greaterthan.addEventListener("click", () => {
   greaterthan.style.display = "none"
-  // lessthan.style.display = "block"
-
-  // navBrand.style.display = "block"
+  leftUl.style.width = "15%"
   navBar.style.paddingLeft = "15%"
   leftNavBar.style.width = "15%"
   sectionContent.style.paddingLeft = "15%"
 
-  const timeOut = setTimeout(() => {
-    lessthan.style.display = "block"
+  lessthan.style.display = "block"
 
-    navBrand.style.display = "block"
+  navBrand.style.display = "block"
 
-    for (let i = 0; i < navName.length; i++) {
-      navName[i].style.display = "block"
-    }
-    for (let i = 0; i < navItems.length; i++) {
-      navItems[i].style.width = "185px"
-    }
-  }, 100)
+  for (let i = 0; i < navName.length; i++) {
+    navName[i].style.display = "block"
+  }
+  for (let i = 0; i < navItems.length; i++) {
+    navItems[i].style.width = "15%"
+  }
+})
+
+// Toggle-button
+
+let toggleButton = document.getElementById("toggle-button")
+let crossButton = document.getElementById("cross-btn")
+
+toggleButton.addEventListener("click", () => {
+  toggleButton.style.display = "none"
+  crossButton.style.display = "block"
+  leftNavBar.style.left = "0"
+})
+
+crossButton.addEventListener("click", () => {
+  crossButton.style.display = "none"
+  toggleButton.style.display = "block"
+  leftNavBar.style.left = "-600px"
 })
